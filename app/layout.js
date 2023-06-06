e8210f6207c8bbde2b79fcac4976b8d1fb0e82fb
arsallanShahab <mdarsallan@gmail.com>
2023-06-06 12:19:05 +0530

    removed the metadata from client component

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,9 @@
 "use client";
 
 import Navbar from "@/components/Navbar";
-
 import Provider from "@/components/Provider";
-import { ToastProvider } from "@/components/ui/toast";
 import { Toaster } from "@/components/ui/toaster";
-import { SessionProvider } from "next-auth/react";
 import "./globals.css";
-
-export const metadata = {
-  title: "Shortner - a url shortner app",
-  description: "A url shortner",
-};
 
 export default function RootLayout({ children, session }) {
   return (
