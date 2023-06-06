34d42ca18de09ad4ba41da5744a600cff11c4fdf
arsallanShahab <mdarsallan@gmail.com>
2023-06-06 15:24:40 +0530

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,26 @@
 import Navbar from "@/components/Navbar";
 import Provider from "@/components/Provider";
 import { Toaster } from "@/components/ui/toaster";
+import Script from "next/script";
 import "./globals.css";
 
 export default function RootLayout({ children, session }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6RYR6WVE2Z"
+          strategy="afterInteractive"
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6RYR6WVE2Z');`}
+        </Script>
+      </head>
       <body className="font-inter">
         <Provider>
           <Navbar />
